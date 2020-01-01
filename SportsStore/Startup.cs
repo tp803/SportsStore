f4312456c6913bdbb9dc5c1dd9f54fd7c5dc824a
tp803 <42017964+tp803@users.noreply.github.com>
2019-12-31 19:27:24 -0600

    Just did some styling and shit

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -39,7 +39,11 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
+                    name: "pagination",
+                    template: "Products/Page{productPage}",
+                    defaults: new { Controller = "Product", action = "List" });
+                routes.MapRoute(
+                    name: "default", 
                     template: "{controller=Product}/{action=List}/{id?}");
             });
             SeedData.EnsurePopulated(app);
