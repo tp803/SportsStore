6b9648af6916ac475570d3bae0eda08ad6cd6f0f
tp803 <42017964+tp803@users.noreply.github.com>
2020-01-03 08:21:13 -0600

    Completed Chapter 9, including getting cart to work

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -6,12 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using SportsStore.Infrastructure;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SportsStore.Controllers
 {
-    /*public class CartController : Controller
+    public class CartController : Controller
     {
         private IProductRepository repository;
 
@@ -20,24 +21,53 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public RedirectToActionResult AddToCart(int productID, string returnUrl)
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
         {
             Product product = repository.Products
-                .FirstOrDefault(p => p.ProductID == productID);
+                .FirstOrDefault(p => p.ProductID == productId);
 
             if(product != null)
             {
                 Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                saveCart(cart);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
             }
 
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        private Cart GetCart()
         {
-
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
         }
-    }*/
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+    }
 }
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -65,7 +65,7 @@ namespace SportsStore
                     defaults: new { controller = "Product", action = "List", productPage = 1 }
                     );
 
-                routes.MapRoute(name: null, template: "{controller}/{action}/{id}");
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
 
             });
             SeedData.EnsurePopulated(app);
